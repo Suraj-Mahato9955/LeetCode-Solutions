b35eff2c4106b19a4a5ed88f13073943dd0eb09c
Suraj <mahatosuraj6224@gmail.com>
2026-09-09 20:57:05 +0530

    Time: 0 ms (100.00%) | Memory: 38.3 MB (83.71%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
+// Suraj Mahato 2420497
     int leastInterval(vector<char>& tasks, int n) {
         unordered_map<char,int>mp;
         int count = 0;
+
+        // Suraj Mahato 2420497
+
         for(auto e : tasks)
         {
             mp[e]++;
             count = max(count, mp[e]);
         }
         
+        // Suraj Mahato 2420497
+
+        
         int ans = (count-1)*(n+1);
         for(auto e : mp) if(e.second == count) ans++;
         return max((int)tasks.size(), ans);
+
+        // Suraj Mahato 2420497
+
     }
 };
