bbfdf72f7835b5ec7bbb364142314b0b25fd5caf
Suraj <mahatosuraj6224@gmail.com>
2026-09-14 22:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 100.4 MB (67.99%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        bool inc = true;
+        bool dec = true;
+
+        for(int i=0; i<=n-2; i++) {
+            if(nums[i]<nums[i+1]) {
+                dec = false;
+
+            }
+        }
+        for(int i=0; i<=n-2; i++) {
+            if(nums[i]>nums[i+1]) {
+                inc = false;
+            }
+        }
+
+        if( inc == true || dec == true) {
+            return true;
+        
+        } else {
+            return false;
+        } 
+        
+    }
+};
